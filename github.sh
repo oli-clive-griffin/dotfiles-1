c5f74edad4aca5a96c46828ea5ad9018d9429156
chry-santhemum <zifanawang04@gmail.com>
2025-04-13 15:30:00 +0000

    commit message

---
+++ github.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Input arguments
+email=${1:-"zifanawang04@gmail.com"}
+name=${2:-"chry-santhemum"}
+github_url=${3:-""}
+
+# Setup git
+git config --global user.email "$email"
+git config --global user.name "$name"
---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 1) Install packages
+apt-get update
+apt-get install -y tmux
+
+# 2) Add aliases and environment variables to .bashrc
+echo "alias gc='git add . && git commit -m'" >> /root/.bashrc
+echo "alias tma='tmux attach -t'" >> /root/.bashrc
+echo "alias venv='source /root/.venv/bin/activate'" >> /root/.bashrc
+
+echo "export HF_HOME=/workspace/hf" >> /root/.bashrc
+echo "export HF_HUB_ENABLE_HF_TRANSFER=1" >> /root/.bashrc
+
+
+# Source
+source /root/.bashrc
+
+# 3) Set up huggingface cache at /workspace/hf
+uv pip install -U "huggingface_hub[cli]"
+uv pip install -U "huggingface-hub[hf-transfer]"
+uv pip install -U wandb
+source .venv/bin/activate
+
+huggingface-cli login --token $HF_TOKEN --add-to-git-credential
+wandb login $WANDB_TOKEN
