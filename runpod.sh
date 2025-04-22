b578c4249f9fb2fd1efd4593d5347515a68279ed
chry-santhemum <zifanawang04@gmail.com>
2025-04-22 16:17:38 +0000

    bleh

--- runpod.sh
+++ runpod.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
 
-# 1) Install uv and create virtual environment
-curl -L https://astral.sh/uv/install.sh | sh
-source $HOME/.local/bin/env
-uv python install 3.11
-uv venv
-
-# 2) Assuming repo is cloned, run setup scripts
-bash dotfiles/github.sh
-source dotfiles/install.sh
-
-# 3) Clone the current working repo
-# uv pip install -r requirements.txt
-
 # install node for vscode
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.2/install.sh | bash
 # in lieu of restarting the shell
@@ -24,3 +11,13 @@ node -v # Should print "v22.14.0".
 nvm current # Should print "v22.14.0".
 # Verify npm version:
 npm -v # Should print "10.9.2".
+
+# 1) Install uv and create virtual environment
+curl -L https://astral.sh/uv/install.sh | sh
+source $HOME/.local/bin/env
+uv python install 3.11
+uv venv
+
+# 2) Assuming repo is cloned, run setup scripts
+bash dotfiles/github.sh
+source dotfiles/install.sh
